ff889b7f51dbc8419361e31b60612c863de3eac3
mhfaisalbd <bdmhfaisal@gmail.com>
2021-01-24 21:03:23 +0600

    Random Line Generating

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ window.onload = function(){
         context = canvas.getContext('2d'),
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
-        
-    context.fillRect(0,0, width, height);
+
+    for (let i = 0; i < 100; i++) {
+        context.beginPath();
+        context.moveTo(Math.random() * width, Math.random() * height);
+        context.lineTo(Math.random() * width, Math.random() * height);
+        context.stroke();
+    }
 }
