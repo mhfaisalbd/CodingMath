1419e2966270d7c4134945461bb074927d7e30b5
mhfaisalbd <bdmhfaisal@gmail.com>
2021-01-24 22:17:12 +0600

    Animation with Sine 02

--- js/tri02.js
+++ js/tri02.js
@@ -6,17 +6,23 @@ window.onload = function(){
 
     let centerY = height * .5,
         centerX = width * .5,
-        offset = height * .4,
+        baseRadious = 200,
+        baseAlpha = 0.5,
+        offset = 100,
+        offsetAlpha = 0.5,
         speed = 0.025,
         angle = 0;
     
     render();
     
     function render(){
-        let y = centerY + Math.sin(angle) * offset;
+        let radious = baseRadious + Math.sin(angle) * offset;
+        let alpha = baseAlpha + Math.sin(angle) * offsetAlpha;
+        
+        context.fillStyle = "rgba(0,0,128,"+alpha+")";
         context.clearRect(0,0, width, height);
         context.beginPath();
-        context.arc(centerX, y, 50, 0, Math.PI * 2, false);
+        context.arc(centerX, centerY, radious, 0, Math.PI * 2, false);
         context.fill();
 
         angle += speed;
