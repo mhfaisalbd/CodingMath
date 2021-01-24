5bd05b129b66007f99e68c746f532d683a139de4
mhfaisalbd <bdmhfaisal@gmail.com>
2021-01-24 23:03:40 +0600

    Arrange object circularly 02

--- js/circle.js
+++ js/circle.js
@@ -4,25 +4,42 @@ window.onload = function(){
         width = canvas.width = window.innerWidth,
         height = canvas.height = window.innerHeight;
 
+    // let centerY = height * .5,
+    //     centerX = width * .5,
+    //     radious = 200,
+    //     speed = 0.025,
+    //     angle = 0;
+    
+    // render();
+    
+    // function render(){
+    //     context.clearRect(0,0, width, height);
+        
+    //     let x = centerX + Math.cos(angle) * radious,
+    //     y = centerY + Math.sin(angle) * radious;
+        
+    //     context.beginPath();
+    //     context.arc(x, y, 10, 0, Math.PI * 2, false);
+    //     context.fill();
+
+    //     angle += speed;
+    //     requestAnimationFrame(render);
+    // }
+
     let centerY = height * .5,
         centerX = width * .5,
         radious = 200,
         speed = 0.025,
-        angle = 0;
+        noOfObject = 480,
+        slice = Math.PI * 2 / noOfObject;
     
-    render();
-    
-    function render(){
-        context.clearRect(0,0, width, height);
-        
-        let x = centerX + Math.cos(angle) * radious,
+    for (let i = 0; i < noOfObject; i++) {
+        let angle = i * slice,
+        x = centerX + Math.cos(angle) * radious,
         y = centerY + Math.sin(angle) * radious;
         
         context.beginPath();
-        context.arc(x, y, 10, 0, Math.PI * 2, false);
+        context.arc(x, y, 2, 0, Math.PI * 2, false);
         context.fill();
-
-        angle += speed;
-        requestAnimationFrame(render);
     }
 }
